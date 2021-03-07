837a08d93c17bbc41706c7c3adbdda546719f751
Quincy <kuncc@foxmail.com>
2021-03-07 17:25:34 +0800

    update: 完成 Sidebar 组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Sidebar/>
     <router-view/>
   </div>
 </template>
@@ -7,17 +8,37 @@
 <script>
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator'
+import Sidebar from '@/components/Sidebar'
 
-@Component
+@Component({
+  components: {Sidebar}
+})
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import "~@/assets/styles/reset.scss";
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 14px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background: #eee;
+}
+
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background-color: #fff;
+  box-shadow: 0 0 2px 0 #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+
+#app {
+  display: flex;
+  align-items: stretch;
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="sidebar">
+    <div class="link">
+      <router-link to="/note/1" title="笔记">笔记</router-link>
+      <router-link to="/notebooks" title="笔记本">笔记本</router-link>
+      <router-link to="/trash/2" title="回收站">回收站</router-link>
+    </div>
+    <div class="logout">
+      返回
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Sidebar extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#sidebar {
+  position: relative;
+  width: 56px;
+  height: 100vh;
+  text-align: center;
+  background: #2c333c;
+  > .link {
+    margin-top: 15px;
+    > a {
+      padding: 6px 0;
+      display: block;
+    }
+    & .router-link-active {
+      background: #5e6266;
+    }
+  }
+  > .logout {
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    cursor: pointer;
+  }
+}
+</style>
