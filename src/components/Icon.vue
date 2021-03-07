3e1a842caefe69a5251a67968f7a36b57fbad856
Quincy <kuncc@foxmail.com>
2021-03-07 19:06:58 +0800

    update

---
+++ src/components/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg>
+    <use :xlink:href="'#'+name"/>
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+@Component
+export default class Icon extends Vue {
+  @Prop() name!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Icon from '@/components/Icon.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component('Icon', Icon);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
